fa0995df87c13b098d84f6a8acae6c6e70bd38c8
sk8erpunk <lori.ramal@gmail.com>
2018-12-02 22:29:16 +0200

    Updating files and adding more

--- Merge Two Sorted Lists.cpp
+++ Merge Two Sorted Lists.cpp
@@ -20,35 +20,19 @@ Output: 1->1->2->3->4->4
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
-        ListNode* head = new ListNode(0);
-        head->next = NULL;
-        ListNode* it1 = l1, *it2 = l2;
-        ListNode *it = head;
-        
-        while(it1 != NULL && it2 != NULL){
-            int val;
-            if(it1->val <= it2->val){
-                val = it1->val;
-                it1 = it1->next;
+        ListNode* head = new ListNode(0); // dummy head;
+        ListNode* curr = head;
+        while(l1 && l2){
+            if(l1->val <= l2->val){
+                curr->next = l1;
+                l1 = l1->next;
             } else {
-                val = it2->val;
-                it2 = it2->next;
+                curr->next = l2;
+                l2 = l2->next;
             }
-
-            ListNode* node = new ListNode(val);
-            it->next = node;
-            it = it->next;
-            
+            curr=curr->next;
         }
-
-        ListNode* rest = it1 == NULL ? it2 : it1;
-        while(rest != NULL){
-            ListNode* node = new ListNode(rest->val);
-            it->next = node;
-            it = it->next;
-            rest = rest->next;
-        }
+        curr->next = l1 == NULL? l2 : l1;
         return head->next;
     }
 };
